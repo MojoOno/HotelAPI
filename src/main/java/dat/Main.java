45fd600bdb3e1405503ab020b34f17bdd09a4592
MojoOno <Ffranck0406@gmail.com>
2025-03-12 14:07:50 +0100

    Addet the method call for RoomController

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -3,15 +3,18 @@ package dat;
 import dat.config.HibernateConfig;
 import dat.controllers.HotelController;
 import dat.config.ApplicationConfig;
+import dat.controllers.RoomController;
 import jakarta.persistence.EntityManagerFactory;
 import dat.rest.Routes;
 
 public class Main {
     final static EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
-    final static HotelController controller = new HotelController(emf);
+    final static HotelController hotelController = new HotelController(emf);
+    final static RoomController roomController = new RoomController(emf);
 
     public static void main(String[] args) {
-        Routes.setController(controller);
+        Routes.setHotelController(hotelController);
+        Routes.setRoomController(roomController);
 
         ApplicationConfig
                 .getInstance()
