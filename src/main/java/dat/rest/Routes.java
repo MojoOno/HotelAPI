774efe17ae6ea5e0a9e2341adafd7e130ea8d0b9
MojoOno <Ffranck0406@gmail.com>
2025-03-12 14:12:13 +0100

    Added RoomController to routes and fixed endpoints
    Added setter for both controllers

--- src/main/java/dat/rest/Routes.java
+++ src/main/java/dat/rest/Routes.java
@@ -1,40 +1,60 @@
 package dat.rest;
 
 import dat.controllers.HotelController;
+import dat.controllers.RoomController;
 import io.javalin.apibuilder.EndpointGroup;
-import lombok.Data;
 
 import static io.javalin.apibuilder.ApiBuilder.*;
 
 
-public class Routes {
-    private static HotelController controller;
+public class Routes
+{
+    private static HotelController hotelController;
+    private static RoomController roomController;
 
-    public Routes(HotelController controller) {
-        Routes.controller = controller;
+    public Routes(HotelController hotelController, RoomController roomController)
+    {
+        Routes.hotelController = hotelController;
+        Routes.roomController = roomController;
     }
 
-    public static EndpointGroup getRoutes() {
-        return () -> {
-            path("hotel", () -> {
-                get("/", controller::getAllHotels);
-                get("/{id}", controller::getHotelById);
-                post("/", controller::createHotel);
-                put("/{id}", controller::updateHotel);
-                delete("/{id}", controller::deleteHotel);
+    public static EndpointGroup getRoutes()
+    {
+        return () ->
+        {
+            path("hotel", () ->
+            {
+                get("/", hotelController::getAllHotels);
+                get("/{id}", hotelController::getHotelById);
+                post("/", hotelController::createHotel);
+                put("/{id}", hotelController::updateHotel);
+                delete("/{id}", hotelController::deleteHotel);
+
+                path("/{id}/room", () ->
+                {
+                    post(roomController::addRoomToHotel);
+                });
+
+                path("/{id}/rooms", () ->
+                {
+                    get(roomController::getRoomsForHotel);
+                });
             });
-            path("room", () -> {
-                post("/", controller::addRoomToHotel);
-                delete("/{id}", controller::deleteRoom);
-            });
-            path("hotel/{id}/rooms", () -> {
-                get(controller::getRoomsForHotel);
+
+            path("room", () ->
+            {
+                delete("/{id}", roomController::deleteRoom);
             });
         };
     }
 
-    public static void setController(HotelController controller)
+    public static void setHotelController(HotelController hotelController)
     {
-        Routes.controller = controller;
+        Routes.hotelController = hotelController;
+    }
+
+    public static void setRoomController(RoomController roomController)
+    {
+        Routes.roomController = roomController;
     }
 }
