853f75e10399b95ecce86bbb11ba1c56295f7903
MojoOno <Ffranck0406@gmail.com>
2025-03-19 11:45:25 +0100

    from filereader to inputstreamreader

--- src/main/java/dat/services/ReadHotelsFromJson.java
+++ src/main/java/dat/services/ReadHotelsFromJson.java
@@ -12,58 +12,47 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
-public class ReadHotelsFromJson
-{
-    public static void main(String[] args)
-    {
-        try (FileReader fileReader = new FileReader("src/main/resources/hotels_with_rooms.json"))
-        {
-            try(Reader reader = new InputStreamReader(
-                    ReadHotelsFromJson.class.getClassLoader().getResourceAsStream("hotels_with_rooms.json"),
-                    StandardCharsets.UTF_8)) {
-                if (reader == null) {
-                    throw new FileNotFoundException("Resource not found: hotels_with_rooms.json");
-                }
+public class ReadHotelsFromJson {
+    public static void main(String[] args) {
+        try (Reader reader = new InputStreamReader(
+                ReadHotelsFromJson.class.getClassLoader().getResourceAsStream("hotels_with_rooms.json"),
+                StandardCharsets.UTF_8)) {
+            if (reader == null) {
+                throw new FileNotFoundException("Resource not found: hotels_with_rooms.json");
             }
-            // Read poems from json file
+
+            // Read hotels from json file
             ObjectMapper objectMapper = new ObjectMapper();
-            HotelJsonWrapper hotelJsonWrapper = objectMapper.readValue(fileReader, HotelJsonWrapper.class);
+            HotelJsonWrapper hotelJsonWrapper = objectMapper.readValue(reader, HotelJsonWrapper.class);
             List<Hotel> hotels = hotelJsonWrapper.getHotels();
             EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
             GenericDAO genericDao = GenericDAO.getInstance(emf);
 
-            for (Hotel hotel : hotels)
-            {
+            for (Hotel hotel : hotels) {
                 genericDao.create(hotel);
-                for (Room room : hotel.getRooms())
-                {
+                for (Room room : hotel.getRooms()) {
                     room.setHotel(hotel);
                     genericDao.create(room);
-
                 }
-
             }
-            //emf.close();
-
-        }
-        catch (Exception e)
-        {
+            // emf.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
     @Getter
     @Setter
     @NoArgsConstructor
     @AllArgsConstructor
-    private static class HotelJsonWrapper
-    {
+    private static class HotelJsonWrapper {
         private List<Hotel> hotels;
     }
 }
